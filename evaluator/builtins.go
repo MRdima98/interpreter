b87920c8dcb39aa9e9a47b1c599c62f5ed384724
MRdima98 <dimafrunza69@gmail.com>
2024-09-02 18:06:03 +0200

    Good effort

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -6,7 +6,7 @@ import (
 )
 
 var builtins = map[string]*object.Builtin{
-	"len": &object.Builtin{
+	"len": {
 		Fn: func(args ...object.Object) object.Object {
 			if len(args) != 1 {
 				return newError("wrong number of arguments. got=%d, want=1",
@@ -14,6 +14,9 @@ var builtins = map[string]*object.Builtin{
 			}
 
 			switch arg := args[0].(type) {
+			case *object.Array:
+				return &object.Integer{Value: int64(len(arg.Elements))}
+
 			case *object.String:
 				return &object.Integer{Value: int64(len(arg.Value))}
 
@@ -21,6 +24,26 @@ var builtins = map[string]*object.Builtin{
 				return newError("argument to `len` not supported, got %s",
 					args[0].Type())
 			}
+		},
+	},
+	"first": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1",
+					len(args))
+			}
+
+			if args[0].Type() != object.ObjectType() {
+				return newError("arguments to `first` must be ARRAY, got %s",
+					args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			if len(arr.Elements) > 0 {
+				return arr.Elements[0]
+			}
+
+			return NULL
 		},
 	},
 }
