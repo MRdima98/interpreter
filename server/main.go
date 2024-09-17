045e7cf6bf85d539eea1a987dec2c0bdb95cddfa
MRdima98 <dimafrunza69@gmail.com>
2024-09-17 15:18:14 +0200

    Set up dev env

--- server/main.go
+++ server/main.go
@@ -1,28 +1,32 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"html/template"
 	"log"
 	"net/http"
 )
+
+const (
+	index = "index.html"
+)
+
+var templates = template.Must(template.ParseFiles(index))
 
 type Gotem struct {
 	Titty string
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("index.html")
+	err := templates.ExecuteTemplate(w, index, Gotem{Titty: "hihi"})
 	if err != nil {
-		fmt.Println(err)
-	}
-	Test := "test"
-	if err := t.Execute(w, Test); err != nil {
-		fmt.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
 func main() {
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
